b29ebacd2805473733839ddafde97f1834d3bb2f
Ivanina-Nedelcheva <ivanina.gn@gmail.com>
2023-07-30 12:38:16 +0300

    service annotation

--- server/src/main/java/com/app/bicycle/service/impl/BicycleServiceImpl.java
+++ server/src/main/java/com/app/bicycle/service/impl/BicycleServiceImpl.java
@@ -1,6 +1,8 @@
 package com.app.bicycle.service.impl;
 
 import com.app.bicycle.service.BicycleService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BicycleServiceImpl implements BicycleService {
 }
--- server/src/main/java/com/app/bicycle/service/impl/StationServiceImpl.java
+++ server/src/main/java/com/app/bicycle/service/impl/StationServiceImpl.java
@@ -3,9 +3,12 @@ package com.app.bicycle.service.impl;
 import com.app.bicycle.entities.Station;
 import com.app.bicycle.repositories.StationRepository;
 import com.app.bicycle.service.StationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class StationServiceImpl implements StationService {
     private final StationRepository stationRepository;
 
