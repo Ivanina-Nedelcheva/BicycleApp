afab1e0788c7efcc5301475e3a4f82071cc942d0
Ivanina-Nedelcheva <ivanina.gn@gmail.com>
2023-08-12 14:48:40 +0300

    cross origin

--- server/src/main/java/com/app/bicycle/controller/BicycleController.java
+++ server/src/main/java/com/app/bicycle/controller/BicycleController.java
@@ -15,6 +15,7 @@ import java.util.List;
 
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("app/bicycles")
 public class BicycleController {
     @Autowired
--- server/src/main/java/com/app/bicycle/controller/StationController.java
+++ server/src/main/java/com/app/bicycle/controller/StationController.java
@@ -10,14 +10,12 @@ import org.springframework.http.HttpStatusCode;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins="*")
 @RequestMapping("app/stations")
 public class StationController {
     @Autowired
