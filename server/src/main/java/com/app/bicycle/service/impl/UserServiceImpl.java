f4e5aa1d22443f819724df98d6615704e1104787
Ivanina-Nedelcheva <ivanina.gn@gmail.com>
2023-10-08 20:13:11 +0300

    added station to rental

--- server/src/main/java/com/app/bicycle/service/impl/UserServiceImpl.java
+++ server/src/main/java/com/app/bicycle/service/impl/UserServiceImpl.java
@@ -203,7 +203,7 @@ public class UserServiceImpl implements UserService {
         Long minutes = (endTime.getTime() - startTime.getTime()) / (60 * 1000);
         BigDecimal price = BigDecimal.valueOf(minutes * prices.getMinutePrice() + prices.getUnlockPrice());
         userRent.setPrice(price);
-        userRent.setDistance((double) (minutes / 4));
+        userRent.setDistance(minutes / 2.5);
         rentalRepository.save(userRent);
 
         chargeUser(price, user);
@@ -251,6 +251,8 @@ public class UserServiceImpl implements UserService {
                 rentalDTO.setPrice(rental.getPrice());
                 rentalDTO.setFinished(rental.isFinished());
                 rentalDTO.setUser(rental.getUser());
+                rentalDTO.setStation(rental.getStation());
+                rentalDTO.setBicycle(rental.getBicycle());
                 Double minutes = (double) ((rental.getEndTime().getTime() - rental.getStartTime().getTime()) / (60 * 1000));
                 rentalDTO.setMinutes(minutes);
 
