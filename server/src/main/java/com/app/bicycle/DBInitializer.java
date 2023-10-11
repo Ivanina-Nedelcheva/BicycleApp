ba1af01c3e30f0d554d902efadb9a852ada29eea
Ivanina-Nedelcheva <ivanina.gn@gmail.com>
2023-10-12 00:04:27 +0300

    scripts

--- server/src/main/java/com/app/bicycle/DBInitializer.java
+++ server/src/main/java/com/app/bicycle/DBInitializer.java
@@ -3,6 +3,7 @@ package com.app.bicycle;
 import com.app.bicycle.entities.Role;
 import com.app.bicycle.entities.User;
 import com.app.bicycle.enums.UserRole;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
@@ -24,9 +25,7 @@ public class DBInitializer implements CommandLineRunner {
     private PasswordEncoder passwordEncoder;
 
     @Override
-    public void run(String... args) throws Exception {
-
-
+    public void run(String... args) {
         List<Role> userRoles = em.createQuery("from Role ", Role.class).setMaxResults(1).getResultList();
         if (userRoles.isEmpty()) {
             Role ordinary = new Role();
@@ -49,44 +48,44 @@ public class DBInitializer implements CommandLineRunner {
             List<User> userList = em.createQuery("from User", User.class).setMaxResults(1).getResultList();
             if (userList.isEmpty()) {
                 User user1 = new User();
-                user1.setFirstName("Iva");
-                user1.setLastName("Ned");
-                user1.setEmail("iva@gmail.com");
+                user1.setFirstName("Ivanina");
+                user1.setLastName("Nedelcheva");
+                user1.setEmail("iva@test.com");
                 user1.setRole(ordinary);
-                user1.setPhoneNumber("08946");
+                user1.setPhoneNumber("0891324500");
                 user1.setAge(25);
                 user1.setPassword(passwordEncoder.encode("123456"));
                 em.persist(user1);
 
 
                 User user2 = new User();
-                user2.setFirstName("Niki");
-                user2.setLastName("Georg");
-                user2.setEmail("nik@gmail.com");
+                user2.setFirstName("Nikolay");
+                user2.setLastName("Georgiev");
+                user2.setEmail("nikolai@test.com");
                 user2.setRole(admin);
-                user2.setPhoneNumber("08942");
+                user2.setPhoneNumber("0894224875");
                 user2.setAge(35);
                 user2.setPassword(passwordEncoder.encode("123456"));
                 em.persist(user2);
 
                 User user3 = new User();
-                user3.setFirstName("Mil");
-                user3.setLastName("Ned");
-                user3.setEmail("mil@gmail.com");
+                user3.setFirstName("Georgi");
+                user3.setLastName("Paskov");
+                user3.setEmail("georgi@test.com");
                 user3.setRole(observer);
-                user3.setPhoneNumber("089754");
+                user3.setPhoneNumber("0893324500");
                 user3.setAge(29);
                 user3.setPassword(passwordEncoder.encode("123456"));
                 em.persist(user3);
 
                 User user4 = new User();
-                user4.setFirstName("Ive");
-                user4.setLastName("Georg");
-                user4.setEmail("ive@gmail.com");
+                user4.setFirstName("Stoqn");
+                user4.setLastName("Ivanov");
+                user4.setEmail("stoqn@test.com");
                 user4.setRole(tech);
-                user4.setPhoneNumber("08966");
+                user4.setPhoneNumber("0891324922");
                 user4.setAge(37);
-                user4.setPassword(passwordEncoder.encode("456789"));
+                user4.setPassword(passwordEncoder.encode("123456"));
                 em.persist(user4);
             }
 
